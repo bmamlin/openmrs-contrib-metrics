d37bd0d2192e161ccc475389adb87898f0cffd95
Burke Mamlin <burke@openmrs.org>
2023-01-19 21:09:15 -0500

    Avoid removing null references that are escaped

--- postgres/docker-entrypoint-initdb.d/03-load-data.sh
+++ postgres/docker-entrypoint-initdb.d/03-load-data.sh
@@ -5,7 +5,7 @@ do
   printf "Importing data from $filename..."
   zcat "$filename" \
   | jq -rc '.payload |= fromjson | (.other // empty) |= fromjson' \
-  | sed 's/\\u0000//g' \
+  | sed -E 's/([^\\])\\u0000/\1/g' \
   | sed 's/\\/\\\\/g' \
   | psql -U postgres openmrs_metrics -c "copy github (data) from stdin;"
 done
